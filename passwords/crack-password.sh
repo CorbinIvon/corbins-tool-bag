d8528fecb8820469ad635d068b6df88e49482ece
Corbin <corbinivon@gmail.com>
2022-11-11 19:02:17 -0800

    Updated code

--- passwords/crack-password.sh
+++ passwords/crack-password.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-character_array=('A' 'B' 'C' 'D' 'E' 'F' 'G' 'H' 'I' 'J' 'K' 'L' 'M' 'N' 'O' 'P' 'Q' 'R' 'S' 'T' 'U' 'V' 'W' 'X' 'Y' 'Z' 'a' 'b' 'c' 'd' 'e' 'f' 'g' 'h' 'i' 'j' 'k' 'l' 'm' 'n' 'o' 'p' 'q' 'r' 's' 't' 'u' 'v' 'w' 'x' 'y' 'z' '0' '1' '2' '3' '4' '5' '6' '7' '8' '9')
+
+SALT=aaaaaaaa
+# Escape special characters.
+DESIRED_HASH=\$6\$aaaaaaaa\$mdOP7SumCDUT/N1JgclftYBzt2WsQZGDFRfQVNQOVq4XSCCYZme3qPDTYkjEPC3iyQv1MryMC149vz7QeYI1y1
+
+character_array=('A' 'B' 'C' 'D' 'E' 'F' 'G' 'H' 'I' 'J' 'K' 'L' 'M' 'N' 'O' 'P' 'Q' 'R' 'S' 'T' 'U' 'V' 'W' 'X' 'Y' 'Z' 'a' 'b' 'c' 'd' 'e' 'f' 'g' 'h' 'i' 'j' 'k' 'l' 'm' 'n' 'o' 'p' 'q' 'r' 's' 't' 'u' 'v' 'w' 'x' 'y' 'z' '0' '1' '2' '3' '4' '5' '6' '7' '8' '9' '!' '@' '#' '$' '%' '^' '&' '(' ')' '_' '=' '+' ',' '.' '<' '>' '/' '\' '?' '~')
 # Can not be empty
 current_password=""
 current_password_array=("$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9" "${10}" "${11}" "${12}" "${13}" "${14}" "${15}" "${16}" "${17}" "${18}" "${19}" "${20}")
@@ -10,7 +15,7 @@ function_get_next_char(){
   local current_char=$1
   for i in "${!character_array[@]}"; do
     # If current character is last character...
-    if [[ "${character_array[61]}" = "${current_char}" ]]; then
+    if [[ "${character_array[81]}" = "${current_char}" ]]; then
       return 0
     fi
     # If current character exists in list...
@@ -23,26 +28,26 @@ function_get_next_char(){
 }
 function_increment_word(){
 
-  if [ "${current_password_array[0]}" == "${character_array[61]}" ]; then
-    if [ "${current_password_array[1]}" == "${character_array[61]}" ]; then
-      if [ "${current_password_array[2]}" == "${character_array[61]}" ]; then
-        if [ "${current_password_array[3]}" == "${character_array[61]}" ]; then
-          if [ "${current_password_array[4]}" == "${character_array[61]}" ]; then
-            if [ "${current_password_array[5]}" == "${character_array[61]}" ]; then
-              if [ "${current_password_array[6]}" == "${character_array[61]}" ]; then
-                if [ "${current_password_array[7]}" == "${character_array[61]}" ]; then
-                  if [ "${current_password_array[8]}" == "${character_array[61]}" ]; then
-                    if [ "${current_password_array[9]}" == "${character_array[61]}" ]; then
-                      if [ "${current_password_array[10]}" == "${character_array[61]}" ]; then
-                        if [ "${current_password_array[11]}" == "${character_array[61]}" ]; then
-                          if [ "${current_password_array[12]}" == "${character_array[61]}" ]; then
-                            if [ "${current_password_array[13]}" == "${character_array[61]}" ]; then
-                              if [ "${current_password_array[14]}" == "${character_array[61]}" ]; then
-                                if [ "${current_password_array[15]}" == "${character_array[61]}" ]; then
-                                  if [ "${current_password_array[16]}" == "${character_array[61]}" ]; then
-                                    if [ "${current_password_array[17]}" == "${character_array[61]}" ]; then
-                                      if [ "${current_password_array[18]}" == "${character_array[61]}" ]; then
-                                        if [ "${current_password_array[19]}" == "${character_array[61]}" ]; then
+  if [ "${current_password_array[0]}" == "${character_array[81]}" ]; then
+    if [ "${current_password_array[1]}" == "${character_array[81]}" ]; then
+      if [ "${current_password_array[2]}" == "${character_array[81]}" ]; then
+        if [ "${current_password_array[3]}" == "${character_array[81]}" ]; then
+          if [ "${current_password_array[4]}" == "${character_array[81]}" ]; then
+            if [ "${current_password_array[5]}" == "${character_array[81]}" ]; then
+              if [ "${current_password_array[6]}" == "${character_array[81]}" ]; then
+                if [ "${current_password_array[7]}" == "${character_array[81]}" ]; then
+                  if [ "${current_password_array[8]}" == "${character_array[81]}" ]; then
+                    if [ "${current_password_array[9]}" == "${character_array[81]}" ]; then
+                      if [ "${current_password_array[10]}" == "${character_array[81]}" ]; then
+                        if [ "${current_password_array[11]}" == "${character_array[81]}" ]; then
+                          if [ "${current_password_array[12]}" == "${character_array[81]}" ]; then
+                            if [ "${current_password_array[13]}" == "${character_array[81]}" ]; then
+                              if [ "${current_password_array[14]}" == "${character_array[81]}" ]; then
+                                if [ "${current_password_array[15]}" == "${character_array[81]}" ]; then
+                                  if [ "${current_password_array[16]}" == "${character_array[81]}" ]; then
+                                    if [ "${current_password_array[17]}" == "${character_array[81]}" ]; then
+                                      if [ "${current_password_array[18]}" == "${character_array[81]}" ]; then
+                                        if [ "${current_password_array[19]}" == "${character_array[81]}" ]; then
                                           echo "End of brute force! Password not found..."
                                           exit 0
                                         fi
@@ -111,10 +116,6 @@ function_increment_word(){
 
   current_password="${current_password_array[0]}${current_password_array[1]}${current_password_array[2]}${current_password_array[3]}${current_password_array[4]}${current_password_array[5]}${current_password_array[6]}${current_password_array[7]}${current_password_array[8]}${current_password_array[9]}${current_password_array[10]}${current_password_array[11]}${current_password_array[12]}${current_password_array[12]}${current_password_array[13]}${current_password_array[14]}${current_password_array[15]}${current_password_array[16]}${current_password_array[17]}${current_password_array[18]}${current_password_array[19]}"
 }
-
-SALT=JHhtblhc
-# Escape special characters.
-DESIRED_HASH=\$6\$JHhtblhc\$MdOP7SumCDUT/N1JgclftYBzt2WsQZGDFRfQVNQOVq4XSCCYZme3qPDTYkjEPC3iyQv1MryMC149vz7QeYI1y1
 
 while [ ! -f ./password.result ];
 do
