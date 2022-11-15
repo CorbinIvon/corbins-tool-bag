4e3d8f810a67d687a4b1b50e9bf59989172bebfa
Corbin <corbinivon@gmail.com>
2022-11-15 12:47:52 -0800

    insert-user.sh v1.0.1

--- file-management/insert-user/insert-user.sh
+++ file-management/insert-user/insert-user.sh
@@ -10,22 +10,27 @@ cat passwd
 read -p "UID: " uid
 read -p "GID: " gid
 read -p "Comment: " comment
-echo "$1:$sha_pass:$uid:$gid:$comment:/home/$1:/bin/bash" >> passwd
+echo "$1:x:$uid:$gid:$comment:/home/$1:/bin/bash" >> passwd
 sudo chmod 644 passwd
 sudo cp passwd $2/etc/passwd
+# etc/shadow
+sudo cp $2/etc/shadow .
+sudo chmod 666 shadow
+sudo echo "$1:$sha_pass:$(date +%s):0:99999:7:::" >> shadow
+sudo chmod 640 shadow
+sudo chown root:shadow shadow
+sudo cp shadow $2/etc/shadow
 # etc/group
 sudo cp $2/etc/group .
 sudo chmod 666 group
 echo "$1:x:$gid:" >> group
 sudo chmod 644 group
 sudo cp group $2/etc/group
+# /etc/sudoers
+# UNIMPLIMENTED!
 # /home/user
-sudo mkdir $2/home/$1
-sudo cp .bashrc $2/home/$1
+sudo cp -r skel/ $2/home/
+sudo mv $2/home/skel $2/home/$1
 sudo chown $1:$1 $2/home/$1
-sudo chown $1:$1 $2/home/$1/.bashrc
-# /etc/skel
-sudo cp -r /skel/* $2/home/$1/
-
 # clean up
-sudo rm passwd group
+sudo rm passwd group shadow
