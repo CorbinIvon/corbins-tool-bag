82b1bf68f238ec530e72bea5b3aab8994f0daadd
Corbin <corbinivon@gmail.com>
2022-11-15 11:27:00 -0800

    Added base profile

--- file-management/insert-user/insert-user.sh
+++ file-management/insert-user/insert-user.sh
@@ -9,8 +9,8 @@ sudo chmod 666 passwd
 cat passwd
 read -p "UID: " uid
 read -p "GID: " gid
-read -p "Comment: " gid
-echo "$1:$sha_pass:$uid:$gid:/home/$1:/bin/bash" >> passwd
+read -p "Comment: " comment
+echo "$1:$sha_pass:$uid:$gid:$comment:/home/$1:/bin/bash" >> passwd
 sudo chmod 644 passwd
 sudo cp passwd $2/etc/passwd
 # etc/group
@@ -21,6 +21,11 @@ sudo chmod 644 group
 sudo cp group $2/etc/group
 # /home/user
 sudo mkdir $2/home/$1
-cp .bashrc $2/home/$1
-chown $1:$1 $2/home/$1
-chown $1:$1 $2/home/$1/.bashrc
+sudo cp .bashrc $2/home/$1
+sudo chown $1:$1 $2/home/$1
+sudo chown $1:$1 $2/home/$1/.bashrc
+# /etc/skel
+sudo cp -r /skel/* $2/home/$1/
+
+# clean up
+sudo rm passwd group
