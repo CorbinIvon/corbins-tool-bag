f6f53357122b00426804d1cc8c9bdbb090e730db
Corbin <corbinivon@gmail.com>
2022-11-15 11:15:36 -0800

    insert-user.sh v1.0.0

--- file-management/insert-user/insert-user.sh
+++ file-management/insert-user/insert-user.sh
@@ -2,9 +2,25 @@
 # USAGE: ./insert-user.sh [$1 USER NAME] [$2 TARGET ROOT DIR]
 
 # Create passwd
-
-# Copy etc/passwd
+sha_pass=$(mkpasswd -m sha-512)
+# etc/passwd
 sudo cp $2/etc/passwd .
 sudo chmod 666 passwd
-echo "$1" >> passwd
+cat passwd
+read -p "UID: " uid
+read -p "GID: " gid
+read -p "Comment: " gid
+echo "$1:$sha_pass:$uid:$gid:/home/$1:/bin/bash" >> passwd
 sudo chmod 644 passwd
+sudo cp passwd $2/etc/passwd
+# etc/group
+sudo cp $2/etc/group .
+sudo chmod 666 group
+echo "$1:x:$gid:" >> group
+sudo chmod 644 group
+sudo cp group $2/etc/group
+# /home/user
+sudo mkdir $2/home/$1
+cp .bashrc $2/home/$1
+chown $1:$1 $2/home/$1
+chown $1:$1 $2/home/$1/.bashrc
