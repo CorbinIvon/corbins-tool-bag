fdb3ec218000138a4833ff9a5c7241fd821f36ee
Corbin <corbinivon@gmail.com>
2022-11-21 08:35:43 -0800

    Added more networking tools

--- networking/ips/get-ips-from-input.sh
+++ networking/ips/get-ips-from-input.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # USAGE: ./get-ips-from-input.sh [FILE WITH IPs]
-grep -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' $1 | sed 's/Nmap scan report for //g' >> ips.txt
+grep -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' $1 | sed 's/Nmap scan report for //g'
