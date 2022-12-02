e3fd1c50ec863c502825149a6e58fdba4a0704e9
Corbin <corbinivon@gmail.com>
2022-12-01 16:36:11 -0800

    Added dns sample

--- networking/ping-device.sh
+++ networking/ping-device.sh
@@ -1,7 +1,12 @@
-result=$(ping -c 1 -w 1 192.168.1.204 | grep "1 received")
+# Can also ping by dns. Corbin-Phone would work.
+# use nslookup to make sure you have the correct name
+#result=$(ping -c 1 -w 1 192.168.1.204 | grep "1 received")
+result=$(ping -c 1 -w 1 Phone-Corbin | grep "1 received")
 if [ -z "$result"  ]
 then
-	echo "device is offline"
+	#echo "device is offline"
+	echo "false"
 else
-	echo "device is online"
+	#echo "device is online"
+	echo "true"
 fi
